c251757e51586f0229bd55a2306acb13199e17d3
Zulian <luiz.zulian@paketa.com.br>
2021-08-29 19:31:42 -0300

    resolucao

--- lib/list_length.ex
+++ lib/list_length.ex
@@ -1,5 +1,12 @@
 defmodule ListLength do
-  def call() do
-    #TO DO
+  def call list do
+    call list, 0
   end
+
+  defp call([], count), do: count
+
+  defp call [_head | tail], count do
+    call tail,(count+1)
+  end
+
 end
